4a63ad5d02fc2bd75ecbe375f6b1d64ada71e468
jrcovell <jrcovell42@gmail.com>
2025-02-10 13:21:02 -0500

    change

--- src/services/apiWeather.js
+++ src/services/apiWeather.js
@@ -4,12 +4,15 @@ export async function getTodaysWeather() {
  
 // const openWeatherApiKey = process.env.WEATHER_KEY;
 const openWeatherApiKey = import.meta.env.VITE_WEATHER_KEY;
-  const lat = 40.1533114942667;
+if (!openWeatherApiKey) {
+  throw new Error('VITE_WEATHER_KEY is not defined');
+}
+const lat = 40.1533114942667;
   const lon = -75.48946578396848;
   console.log('test')
   console.log(import.meta.env.VITE_WEATHER_KEY);
   // console.log(process.env.WEATHER_KEY);
-  const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}&units=imperial`;
+  const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=$2e1b1808400c0b4f18f88d853977d29c&units=imperial`;
   // https://api.openweathermap.org/data/2.5/weather?lat=40.1533114942667&lon=-75.48946578396848&appid=2e1b1808400c0b4f18f88d853977d29c
   const response = await fetch(openWeatherUrl);
 

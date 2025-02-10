421c60432439f938da852817aa000044b8da6c51
jrcovell <jrcovell42@gmail.com>
2025-02-10 13:17:03 -0500

    tt

--- src/services/apiWeather.js
+++ src/services/apiWeather.js
@@ -7,6 +7,7 @@ const openWeatherApiKey = import.meta.env.VITE_WEATHER_KEY;
   const lat = 40.1533114942667;
   const lon = -75.48946578396848;
   
+  console.log(import.meta.env.VITE_WEATHER_KEY);
   // console.log(process.env.WEATHER_KEY);
   const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${openWeatherApiKey}&units=imperial`;
   // https://api.openweathermap.org/data/2.5/weather?lat=40.1533114942667&lon=-75.48946578396848&appid=2e1b1808400c0b4f18f88d853977d29c
